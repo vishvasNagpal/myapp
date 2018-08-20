9cc904ce4216b1908315bcdd3775d129d136c3e6
vishvasNagpal <vishvas.nagpal@gmail.com>
2018-08-20 16:11:40 +0530

    adding user

---
+++ server/dummyData/users.js
@@ -0,0 +1,344 @@
+const users = {
+    'preeti': {
+        briefDetails: {
+            firstName: 'Preeti',
+            lastName: 'Hans',
+            work: {
+                companyName: 'Josh Technologies',
+                position: 'Sr. Software Developer',
+            },
+            profileViews: {
+                count: 121,
+            },
+            connectionsCount: 673,
+        },
+        allDetails: {
+            educationDetails: [
+                {
+                    instituteName: 'Swami devi dyal inst. of engg.',
+                    degreeName: 'Master of Computer Applications (MCA)',
+                    fieldOfStudy: 'Computer Software Engineering',
+                    grade: 'Grade',
+                    from: '2010',
+                    to: '2013',
+                }
+            ],
+            experience: [
+                {
+                    title: 'Engineer III',
+                    companyName: 'WalmartLabs India',
+                    location: 'Bengaluru, Karnataka, India',
+                    from: {
+                        month: 7,
+                        year: 2018,
+                    },
+                    to: {
+                        month: null,
+                        year: null,
+                    },
+                    current: true,
+                    description: null,
+                },
+                {
+                    title: 'Senior Frontend Developer',
+                    companyName: 'limestone india',
+                    location: 'Gurgaon, Haryana, India',
+                    from: {
+                        month: 6,
+                        year: 2017,
+                    },
+                    to: {
+                        month: 8,
+                        year: 2018,
+                    },
+                    current: false,
+                    description: null,
+                },
+                {
+                    title: 'Senior Frontend Developer',
+                    companyName: 'Wize Commerce Private Limited',
+                    location: 'Gurgaon, Haryana, India',
+                    from: {
+                        month: 8,
+                        year: 2015,
+                    },
+                    to: {
+                        month: 7,
+                        year: 2017,
+                    },
+                    current: false,
+                    description: `With Wize Commerce, I was involved in a project called Nextag. Which is a Price Comparison Engine. My Role in this project was a Frontend Developer Which includes Making the Architectural Application Design, Defining the structure, Managing the data from server, Leading the team.
+
+                    Technologies used: Nodejs, ReactJS, ExpressJs, Javascript, MVC, Sass, CSS3, ECMA6, JSON, Ajax.`,
+                },
+            ],
+            skills: {
+                primarySkills: [
+                    {
+                        skill: 'Node.js',
+                        endorsementCount: 7,
+                    },
+                    {
+                        skill: 'React.js',
+                        endorsementCount: 6,
+                    },
+                    {
+                        skill: 'Redux',
+                        endorsementCount: 6,
+                    },
+                ],
+                industrySkills: [
+                    {
+                        skill: 'Web Development',
+                        endorsementCount: 4,
+                    },
+                    {
+                        skill: 'Mobile Applications',
+                        endorsementCount: 8,
+                    },
+                ],
+                technologySkills: [
+                    {
+                        skill: 'object oriented javascript',
+                        endorsementCount: 8,
+                    },
+                    {
+                        skill: 'Jquery',
+                        endorsementCount: 8,
+                    },
+                    {
+                        skill: 'Javascript',
+                        endorsementCount: 8,
+                    },
+                    {
+                        skill: 'HtML',
+                        endorsementCount: 8,
+                    },
+                    {
+                        skill: 'CSS',
+                        endorsementCount: 8,
+                    },
+                    {
+                        skill: 'Bootstrap',
+                        endorsementCount: 8,
+                    },
+                    {
+                        skill: 'Json',
+                        endorsementCount: 8,
+                    },
+                    {
+                        skill: 'Git',
+                        endorsementCount: 8,
+                    },
+                ],
+
+                otherSkills: [
+                    {
+                        skill: 'ExpressJs',
+                        endorsementCount: null,
+                    },
+                    {
+                        skill: 'CSS3',
+                        endorsementCount: null,
+                    },
+                    {
+                        skill: 'MVC',
+                        endorsementCount: undefined,
+                    },
+                    {
+                        skill: 'Mobile First',
+                        endorsementCount: 8,
+                    },
+                    {
+                        skill: 'Responsive Designs',
+                        endorsementCount: 1,
+                    },
+                    {
+                        skill: 'Sencha Touch',
+                        endorsementCount: 8,
+                    },
+                    {
+                        skill: 'Json',
+                        endorsementCount: 8,
+                    },
+                    {
+                        skill: 'Git',
+                        endorsementCount: 8,
+                    },
+                ]
+            }
+        }
+    },
+    'vishvas': {
+        briefDetails: {
+            firstName: 'Vishvas',
+            lastName: 'Nagpal',
+            work: {
+                companyName: 'Walmart Labs',
+                position: 'Engineer 3',
+            },
+            profileViews: {
+                count: 121,
+            },
+            connectionsCount: 673,
+        },
+        allDetails: {
+            educationDetails: [
+                {
+                    instituteName: 'Swami devi dyal inst. of engg.',
+                    degreeName: 'Master of Computer Applications (MCA)',
+                    fieldOfStudy: 'Computer Software Engineering',
+                    grade: 'Grade',
+                    from: '2010',
+                    to: '2013',
+                }
+            ],
+            experience: [
+                {
+                    title: 'Engineer III',
+                    companyName: 'WalmartLabs India',
+                    location: 'Bengaluru, Karnataka, India',
+                    from: {
+                        month: 7,
+                        year: 2018,
+                    },
+                    to: {
+                        month: null,
+                        year: null,
+                    },
+                    current: true,
+                    description: null,
+                },
+                {
+                    title: 'Senior Frontend Developer',
+                    companyName: 'limestone india',
+                    location: 'Gurgaon, Haryana, India',
+                    from: {
+                        month: 6,
+                        year: 2017,
+                    },
+                    to: {
+                        month: 8,
+                        year: 2018,
+                    },
+                    current: false,
+                    description: null,
+                },
+                {
+                    title: 'Senior Frontend Developer',
+                    companyName: 'Wize Commerce Private Limited',
+                    location: 'Gurgaon, Haryana, India',
+                    from: {
+                        month: 8,
+                        year: 2015,
+                    },
+                    to: {
+                        month: 7,
+                        year: 2017,
+                    },
+                    current: false,
+                    description: `With Wize Commerce, I was involved in a project called Nextag. Which is a Price Comparison Engine. My Role in this project was a Frontend Developer Which includes Making the Architectural Application Design, Defining the structure, Managing the data from server, Leading the team.
+
+                    Technologies used: Nodejs, ReactJS, ExpressJs, Javascript, MVC, Sass, CSS3, ECMA6, JSON, Ajax.`,
+                },
+            ],
+            skills: {
+                primarySkills: [
+                    {
+                        skill: 'Node.js',
+                        endorsementCount: 7,
+                    },
+                    {
+                        skill: 'React.js',
+                        endorsementCount: 6,
+                    },
+                    {
+                        skill: 'Redux',
+                        endorsementCount: 6,
+                    },
+                ],
+                industrySkills: [
+                    {
+                        skill: 'Web Development',
+                        endorsementCount: 4,
+                    },
+                    {
+                        skill: 'Mobile Applications',
+                        endorsementCount: 8,
+                    },
+                ],
+                technologySkills: [
+                    {
+                        skill: 'object oriented javascript',
+                        endorsementCount: 8,
+                    },
+                    {
+                        skill: 'Jquery',
+                        endorsementCount: 8,
+                    },
+                    {
+                        skill: 'Javascript',
+                        endorsementCount: 8,
+                    },
+                    {
+                        skill: 'HtML',
+                        endorsementCount: 8,
+                    },
+                    {
+                        skill: 'CSS',
+                        endorsementCount: 8,
+                    },
+                    {
+                        skill: 'Bootstrap',
+                        endorsementCount: 8,
+                    },
+                    {
+                        skill: 'Json',
+                        endorsementCount: 8,
+                    },
+                    {
+                        skill: 'Git',
+                        endorsementCount: 8,
+                    },
+                ],
+
+                otherSkills: [
+                    {
+                        skill: 'ExpressJs',
+                        endorsementCount: null,
+                    },
+                    {
+                        skill: 'CSS3',
+                        endorsementCount: null,
+                    },
+                    {
+                        skill: 'MVC',
+                        endorsementCount: undefined,
+                    },
+                    {
+                        skill: 'Mobile First',
+                        endorsementCount: 8,
+                    },
+                    {
+                        skill: 'Responsive Designs',
+                        endorsementCount: 1,
+                    },
+                    {
+                        skill: 'Sencha Touch',
+                        endorsementCount: 8,
+                    },
+                    {
+                        skill: 'Json',
+                        endorsementCount: 8,
+                    },
+                    {
+                        skill: 'Git',
+                        endorsementCount: 8,
+                    },
+                ]
+            }
+        }
+    }
+};
+
+module.exports = users;
--- server/router/UserRouter.js
+++ server/router/UserRouter.js
@@ -1,44 +1,22 @@
 const AbstractRouter = require('./AbstractRouter');
+const users = require('../dummyData/users');
 
 class UserRouter extends AbstractRouter{
     get(req,res){
-        let user = {}
-        switch ((req.query.useName).toLowerCase()) {
-            case 'vishvas' :
-                user = {
-                    firstName: 'Vishvas',
-                    lastName: 'Nagpal',
-                    work: {
-                        companyName: 'Walmart Labs',
-                        position: 'Engineer 3',
-                    },
-                    profileViews: {
-                        count: 121,
-                    }
-                }
-                break;
-            case 'preeti' :
-                user = {
-                    firstName: 'Preeti',
-                    lastName: 'Hans',
-                    work: {
-                        companyName: 'Josh Technologies',
-                        position: 'Sr. Software Engineer',
-                    },
-                    profileViews: {
-                        count: 121,
-                    }
-                }
-                break;
-
-            default :
-                user = {
-                    message: 'User not found',
-                }
-                break;
+        const userName = req.query.userName.toLowerCase();
+        let response;
+        if(users[userName]){
+            if (req.query.allDetails) {
+                response = users[userName];
+            } else {
+                response = users[userName].briefDetails;
+            }
+        } else {
+            response = {
+                message: 'user not found'
+            }
         }
-
-        res.json(user);
+        res.json(response);
     }
 }
 
